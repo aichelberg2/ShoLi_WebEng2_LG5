9a9b5d13aaf3babe40fea1c7963471407985e5fb
aichelberg2 <aichelberg2@gmail.com>
2022-04-01 20:31:19 +0200

    Fixed routing problem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthenticationGuard } from "./services/auth/auth-guard-service.guard";
+
+import { HomeComponent } from './home/home.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { SettingsComponent } from './settings/settings.component';
+import { UserSettingsComponent } from './user-settings/user-settings.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 
+
+const routes: Routes = [
+
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'home', component: HomeComponent, canActivate: [AuthenticationGuard] },
+  { path: 'login', component: LogInComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'user-settings', component: UserSettingsComponent },
+  { path: '404', component: NotfoundComponent },
+  { path: '**', redirectTo: '404' },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
