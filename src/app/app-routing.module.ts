da8055aa2f0c02178b107d67f73f808b75015931
aichelberg2 <aichelberg2@gmail.com>
2022-04-01 19:36:46 +0200

    Added Angular Routing Feature

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { SettingsComponent } from './settings/settings.component';
+import { UserSettingsComponent } from './user-settings/user-settings.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LogInComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'user-settings', component: UserSettingsComponent },
+  { path: '404', component: NotfoundComponent },
+  { path: '**', redirectTo: '/404' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
