ba249371d18d74092199fd63085e9dda2c877de2
JewgeniSchnaper <jewgeni_schnaper@online.de>
2022-04-08 10:46:47 +0200

    First Commit, changed directories

---
+++ php/db_connection.php
@@ -0,0 +1,11 @@
+<?php
+$sname = "localhost";
+$uname = "root";
+$password = "";
+$db_name = "sholi";
+
+$conn = mysqli_connect($sname, $uname, $password, $db_name);
+if (!$conn) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
