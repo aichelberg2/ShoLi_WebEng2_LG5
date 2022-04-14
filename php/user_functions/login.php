b4f3a65566acf8abcb52943ca0b4bdbc2df38a87
HermesV4 <lucasspaeth01@gmail.com>
2022-04-14 15:07:10 +0200

    bugfixes

--- php/user_functions/login.php
+++ php/user_functions/login.php
@@ -9,7 +9,7 @@ $inputRaw = file_get_contents("php://input");
 $input = json_decode($inputRaw);
 
 $username = mysqli_real_escape_string($conn, $input->username);
-$password = mysqli_real_escape_string($conn, $input->password);
+$password = mysqli_real_escape_string($conn, $input->pw);
 
 //for security: https://stackoverflow.com/questions/97984/how-to-secure-database-passwords-in-php
 //https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
