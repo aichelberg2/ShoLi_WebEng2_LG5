00756ded003450fc6fb741fb7a127069f76071cb
luxario1 <lucasspaeth01@gmail.com>
2022-04-19 16:22:46 +0200

    updated calls and users for shared List, updated log-out

--- src/app/services/manageUserData/manage-user-data.service.ts
+++ src/app/services/manageUserData/manage-user-data.service.ts
@@ -8,15 +8,31 @@ import {Observable} from "rxjs";
 })
 export class ManageUserDataService {
 
+  private username_loggedIn: string | undefined;
+
   urlCheckUserDataInput_Login: any = "https://sholi.server-welt.com/php/user_functions/login.php";
   urlCheckUserDataInput_Register: any = "https://sholi.server-welt.com/php/user_functions/registration.php";
   urlGetUser: any = "https://sholi.server-welt.com/php/user_functions/getUsers.php";
   urlGetThisUSer: any = "https://sholi.server-welt.com/php/user_functions/getSessionData.php";
 
+  // urlCheckUserDataInput_Login: any = "http://localhost/php/user_functions/login.php";
+  // urlCheckUserDataInput_Register: any = "http://localhost/php/user_functions/registration.php";
+  // urlGetUser: any = "http://localhost/php/user_functions/getUsers.php";
+  // urlGetThisUSer: any = "http://localhost/php/user_functions/getSessionData.php";
+
+
   constructor(private http: HttpClient) {
   }
 
-  getThisUser(){
+  setUsername_loggedIn(name: any) {
+    this.username_loggedIn = name;
+  }
+
+  getUsername_loggedIn() {
+    return this.username_loggedIn
+  }
+
+  getThisUser(): Observable<any> {
     return this.http.get(this.urlGetThisUSer);
   }
 
