2fa70ac65fb6ed4048a37686185e692a9b3ecb45
luxario1 <lucasspaeth01@gmail.com>
2022-04-19 15:37:08 +0200

    testing

--- php/user_functions/getSessionData.php
+++ php/user_functions/getSessionData.php
@@ -5,6 +5,7 @@ header("Access-Control-Allow-Methods: *");
 header("Access-Control-Allow-Headers: *");
 require '../db_connection.php';
 session_start();
-$data = array('username' => $_SESSION['username']);
+$name = $_SESSION['username'];
+$data = array('username' => $name);
 echo json_encode($data);
 ?>
