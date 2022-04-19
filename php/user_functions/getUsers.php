ba5a22821340f5d90cec38438c2bd23116a12b56
Lucas <102356694+luxario1@users.noreply.github.com>
2022-04-19 16:05:44 +0200

    Update getUsers.php

--- php/user_functions/getUsers.php
+++ php/user_functions/getUsers.php
@@ -2,22 +2,15 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: *");
-header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 require '../db_connection.php';
-session_start();
-//$postdata = file_get_contents("php://input");
-//$request = json_decode($postdata);
-//$username = $request->username;
-//$pw = $request->pw;
-$data = array(
-                array('username' => 'lucas123', 'firstname' => 'Lucas', 'eMail' => 'lucas@lucas.de', 'password' => 'test123'),
-                array('username' => 'alex123', 'firstname' => 'Alex', 'eMail' => 'alex@alex.de', 'password' => 'test123'),
-                array('username' => 'jonas123', 'firstname' => 'Jonas', 'eMail' => 'jonas@jonas.de', 'password' => 'test123'),
-                array('username' => 'thomas123', 'firstname' => 'Thomas', 'eMail' => 'thomas@thomas.de', 'password' => 'test123'),
-                array('username' => 'sascha123', 'firstname' => 'Sascha', 'eMail' => 'freddy@freddy.de', 'password' => 'test123'),
-                array('username' => 'jefi123', 'firstname' => 'Jefi', 'eMail' => 'sascha@sascha.de', 'password' => 'test123'),
-                array('username' => 'elli123', 'firstname' => 'Elli', 'eMail' => 'elli@elli.de', 'password' => 'test123'),
-                array('username' => 'chris123', 'firstname' => 'Chris', 'eMail' => 'chris@chris.de', 'password' => 'test123')
-);
-echo json_encode($data);
+
+$query = "SELECT username FROM user";
+$usernames = array();
+$result = mysqli_query($conn,$query) or die(mysqli_error());
+while($row = mysqli_fetch_assoc($result))
+{
+  $usernames[] = $row;
+}
+echo $usernames;
 ?>
