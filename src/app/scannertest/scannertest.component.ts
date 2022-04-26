56476afecbc9d24f5634905c242342ad60652714
aichelberg2 <aichelberg2@gmail.com>
2022-04-26 15:26:12 +0200

    added scanner

--- src/app/scannertest/scannertest.component.ts
+++ src/app/scannertest/scannertest.component.ts
@@ -5,11 +5,101 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './scannertest.component.html',
   styleUrls: ['./scannertest.component.css']
 })
+
 export class ScannertestComponent implements OnInit {
+  private _scannerIsRunning = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.startScanner();
+  }
+
+  startScanner() {
+    const Quagga = require('quagga');
+    try {
+      Quagga.init({
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          target: document.querySelector('#scanner-container'),
+          constraints: {
+            width: 400,
+            height: 400,
+            facingMode: "environment"
+          },
+        },
+        decoder: {
+          readers: [
+            "code_128_reader",
+            "ean_reader",
+            "ean_8_reader",
+            "code_39_reader",
+            "code_39_vin_reader",
+            "codabar_reader",
+            "upc_reader",
+            "upc_e_reader",
+            "i2of5_reader"
+          ],
+          debug: {
+            showCanvas: true,
+            showPatches: true,
+            showFoundPatches: true,
+            showSkeleton: true,
+            showLabels: true,
+            showPatchLabels: true,
+            showRemainingPatchLabels: true,
+            boxFromPatches: {
+              showTransformed: true,
+              showTransformedBox: true,
+              showBB: true
+            }
+          }
+        },
+      });
+    }
+    catch (err) {
+      console.log(err);
+      return
+    }
+    finally {
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+
+      // Set flag to is running
+      this._scannerIsRunning = true;
+    }
+
+    Quagga.onProcessed((result: any) => {
+      var drawingCtx = Quagga.canvas.ctx.overlay,
+        drawingCanvas = Quagga.canvas.dom.overlay;
+
+      if (result) {
+        if (result.boxes) {
+          drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+          result.boxes.filter((box: any) => {
+            return box !== result.box;
+          }).forEach((box: any) => {
+            Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
+          });
+        }
+
+        if (result.box) {
+          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
+        }
+
+        if (result.codeResult && result.codeResult.code) {
+          Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+        }
+      }
+    });
+
+
+    Quagga.onDetected((result: any) => {
+      console.log("Barcode detected and processed : [" + result.codeResult.code + "]", result);
+    });
+
   }
 
 }
+
