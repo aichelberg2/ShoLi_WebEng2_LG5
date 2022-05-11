3d1d075e517af2eef687e031c0bc4430c34d81b6
JewgeniSchnaper <53540832+JewgeniSchnaper@users.noreply.github.com>
2022-05-11 23:18:42 +0200

    isCreator now uses prepared statements

--- php/list_functions/isCreator.php
+++ php/list_functions/isCreator.php
@@ -4,15 +4,20 @@ header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: *");
 header("Access-Control-Allow-Headers: *");
 require '../db_connection.php';
+
 $inputRaw = file_get_contents("php://input");
-//$inputRaw = '{"creator":"lucario1234","listID":"123"}';
+//$inputRaw = '{"creator":"user3","listID":"5"}';
 $input = json_decode($inputRaw);
 
 $creator = mysqli_real_escape_string($conn, $input->creator);
 $listID = mysqli_real_escape_string($conn, $input->listID);
 
-$query = "SELECT * FROM list WHERE creator='$creator' AND list_id='$listID'";
-$result = mysqli_query($conn,$query);
+$stmt = $conn->prepare(   "SELECT *
+                                FROM list
+                                WHERE creator=? AND list_id=?");
+$stmt->bind_param("si", $creator,$listID); // 's' => 'string', 'i' => 'integer', 'd' => 'double'
+$stmt->execute();
+$result = $stmt->get_result();
 if (mysqli_num_rows($result) == 1) {
   echo 1;
 } else {
