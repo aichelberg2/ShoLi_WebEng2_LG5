697c40142d60537c01f096066958364a0a7cb4ee
luxario1 <lucasspaeth01@gmail.com>
2022-05-13 21:01:57 +0200

    - restructured Auth-Guards with sessionStorage - changed User-Adding when a list is shared - reworked reactions when closing modals - reworked user-settings - added remove product from a list - reworked added products to a list

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {ManageProductDataService} from "../services/manageProductData/manage-product-data.service";
-import {FormControl} from "@angular/forms";
+import {FormControl, NgForm} from "@angular/forms";
 import {Observable, retry, share, Subject, switchMap, takeUntil, timer} from "rxjs";
 
 @Component({
@@ -35,9 +35,12 @@ export class ListComponent implements OnInit {
   receivedProductIDOfList: any[] = [];
   receivedProductsOfCategory: any[] = [];
   myControl = new FormControl();
-  selectedProducts: any;
+  selectedProducts: any = [];
+  selectedProductCategory: any = "";
   receivedProductsOfListOberservable: Observable<any> | undefined;
   private stopPolling = new Subject();
+  createNextNewProduct: boolean = false;
+  temporaryProductToDelete: any;
 
   constructor(private route: ActivatedRoute, private manageProductData: ManageProductDataService) {
   }
@@ -85,7 +88,8 @@ export class ListComponent implements OnInit {
 
 
   transmitProductCategorie() {
-    this.isProductKategorieChoosed = true;
+    if (this.choosedProductCategorie)
+      this.isProductKategorieChoosed = true;
     let data = {
       'productCategory': this.choosedProductCategorie
     }
@@ -99,24 +103,111 @@ export class ListComponent implements OnInit {
   }
 
   deleteProductFromList() {
-
-  }
-
-  selectOption(value: any) {
-    console.log(value);
-  }
-
-  addProductsToList() {
-    for (let i = 0; i < this.selectedProducts.length; i++) {
-      this.receivedProductIDOfList.push(this.selectedProducts[i].pr_id);
-      this.receivedProductsOfList.push(this.selectedProducts[i]);
+    let data = {
+      'listID': this.listid,
+      'productID': this.temporaryProductToDelete
     }
+
+    this.manageProductData.removeProduct(data).subscribe(value => {
+      if (value == 1) {
+        for (let i = 0; i < this.receivedProductsOfList.length; i++) {
+          if (this.receivedProductsOfList[i].pr_id == this.temporaryProductToDelete) {
+            const index = this.receivedProductsOfList.indexOf(this.receivedProductsOfList[i], 0);
+            if (index > -1) {
+              this.receivedProductsOfList.splice(index, 1);
+            }
+          }
+        }
+        const index1 = this.receivedProductIDOfList.indexOf(this.temporaryProductToDelete, 0);
+        if (index1 > -1) {
+          this.receivedProductIDOfList.splice(index1, 1);
+        }
+      }
+    })
+  }
+
+  async addProductsToList() {
+    let prIDs: any[] = [];
     console.log(this.selectedProducts)
-    console.log(this.receivedProductsOfList)
-    console.log(this.receivedProductsOfList)
+    for (let i = 0; i < this.selectedProducts.length; i++) {
+      prIDs.push(this.selectedProducts[i].pr_id)
+    }
+    let data = {
+      'listID': this.listid,
+      'productIDs': prIDs
+    }
+    console.log(data)
+    this.manageProductData.addProductToList(data).subscribe(value => {
+      if (value == 1) {
+        for (let i = 0; i < this.selectedProducts.length; i++) {
+          console.log(this.selectedProducts[i])
+          this.receivedProductIDOfList.push(this.selectedProducts[i].pr_id);
+          this.receivedProductsOfList.push(this.selectedProducts[i]);
+        }
+      }
+
+    })
+    this.choosedProductCategorie = undefined;
+    this.isProductKategorieChoosed = false;
   }
 
   ngOnDestroy() {
     this.stopPolling.next("rs");
+  }
+
+  createProduct(newProductName: any, newProductPrice: any, shouldBeAddedToList: any) {
+    let data = {
+      'productName': newProductName,
+      'productCategory': this.selectedProductCategory,
+      'productPrice': newProductPrice
+    }
+    console.log(data)
+    // await new Promise<void>(resolve => {
+    this.manageProductData.createProduct(data).subscribe(value => {
+      if (value != 0) {
+        let temp: any[] = [];
+        if (shouldBeAddedToList) {
+          temp.push(value);
+          let prod = {
+            'listID': this.listid,
+            'productIDs': temp
+          }
+          this.manageProductData.addProductToList(prod).subscribe();
+          // let product = {
+          //   'pr_id': value,
+          //   'name': newProductName,
+          //   'price': newProductPrice,
+          //   'ticked': 0
+          // }
+          // console.log(product)
+          // this.selectedProducts.push(product);
+          // console.log(this.selectedProducts);
+          // this.addProductsToList();
+        }
+      }
+    })
+    //   resolve();
+    // })
+
+    this.createNextNewProduct = true;
+  }
+
+  clearForm(listInfoForm: NgForm) {
+    listInfoForm.resetForm();
+  }
+
+  toggleTicked(pr_id: any, number: number) {
+    let data = {
+      'listID': this.listid,
+      'productID': pr_id,
+      'isTicked': number
+    }
+    this.manageProductData.updateIsTicked(data).subscribe(value => {
+
+    });
+  }
+
+  resetModal(productForm: NgForm) {
+    productForm.resetForm();
   }
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,22 +1,33 @@
 import {Injectable} from '@angular/core';
 import {delay, of} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
 
-  private isloggedIn: boolean = false;
+   //isloggedIn: boolean = false;
+
 
   constructor() {
   }
 
-  setIsLoggedIn(logStatus: boolean) {
-    this.isloggedIn = logStatus;
+  setIsLoggedIn() {
+    sessionStorage.setItem('loggedIn', 'true');
+    //this.isloggedIn = JSON.parse(sessionStorage.getItem('loggedIn') || 'false');
   }
 
+  // setIsLoggedIn(logStatus: boolean) {
+  //   this.isloggedIn = ;
+  //   //sessionStorage.setItem('loggedIn','true');
+  // }
+
   getIsLoggedIn() {
-    return of(this.isloggedIn).pipe(delay(500));
-    //return of(true).pipe(delay(500));
+    return JSON.parse(sessionStorage.getItem('loggedIn') || 'false');
+    //return of(this.isloggedIn).pipe(delay(500));
+    //return JSON.parse(localStorage.getItem('loggedIn')||this.isloggedIn.toString());
+    // return JSON.parse(localStorage.getItem('loggedIn')||'false')
   }
 }
