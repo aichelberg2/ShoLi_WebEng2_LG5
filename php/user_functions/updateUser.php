904f4be0cae01281258090b2f02477dffc92ae12
JewgeniSchnaper <53540832+JewgeniSchnaper@users.noreply.github.com>
2022-05-19 19:51:05 +0200

    fixed updateUser

--- php/user_functions/updateUser.php
+++ php/user_functions/updateUser.php
@@ -5,13 +5,15 @@ header("Access-Control-Allow-Methods: *");
 header("Access-Control-Allow-Headers: *");
 require '../db_connection.php';
 
-//$inputRaw = '{"username":"user2","firstname":"1","lastname":"1","eMail":"1"}';
+//$inputRaw = '{"username":"user2","firstname":"3","lastname":"1","eMail":"a@a.a"}';
 $inputRaw = file_get_contents("php://input");
 $input = json_decode($inputRaw);
 $username = mysqli_real_escape_string($conn, $input->username);
 $firstname = mysqli_real_escape_string($conn, $input->firstname);
 $lastname = mysqli_real_escape_string($conn, $input->lastname);
-$mail = mysqli_real_escape_string($conn, $input->eMail);
+$email = mysqli_real_escape_string($conn, $input->eMail);
+
+
 
 $stmt = $conn->prepare("UPDATE user SET firstname=?, lastname=?, email=? WHERE username=?");
 $stmt->bind_param("ssss", $firstname,$lastname, $email, $username); // 's' => 'string', 'i' => 'integer', 'd' => 'double'
