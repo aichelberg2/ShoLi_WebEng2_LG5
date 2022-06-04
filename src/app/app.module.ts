3461a63bc7f007e4a069f3788b8afa942c959176
Lucas <102356694+luxario1@users.noreply.github.com>
2022-06-04 10:46:39 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { ApplicationInformationComponent } from './application-information/appli
 import { UserSettingsComponent } from './user-settings/user-settings.component';
 import { NgxPopperModule } from 'ngx-popper';
 import { LogOutComponent } from './log-out/log-out.component';
-import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatAutocompleteModule } from "@angular/material/autocomplete";
 import { MatFormFieldModule } from "@angular/material/form-field";
@@ -19,12 +19,16 @@ import { MatInputModule } from "@angular/material/input";
 import { ListComponent } from './list/list.component';
 import { ImpressumComponent } from './impressum/impressum.component';
 import { ScannertestComponent } from './scannertest/scannertest.component';
-import { ScrollingModule } from "@angular/cdk/scrolling";
-import { MatGridListModule } from "@angular/material/grid-list";
-import { MatSelectModule } from "@angular/material/select";
+import {ScrollingModule} from "@angular/cdk/scrolling";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatSelectModule} from "@angular/material/select";
 import { DataprivacyComponent } from './dataprivacy/dataprivacy.component';
-import { MatSnackBar, MatSnackBarModule } from "@angular/material/snack-bar";
-import { AuthInterceptor } from './auth.interceptor';
+import {MatSnackBar, MatSnackBarModule} from "@angular/material/snack-bar";
+import { BackButtonComponent } from './back-button/back-button.component';
+import { WhatDoesApplComponent } from './application-information/what-does-appl/what-does-appl.component';
+import { HowCreateListComponent } from './application-information/how-create-list/how-create-list.component';
+import { HowShareListComponent } from './application-information/how-share-list/how-share-list.component';
+
 
 
 const routes: Routes = [
@@ -37,7 +41,7 @@ const routes: Routes = [
   { path: 'impressum', component: ImpressumComponent },
   { path: 'dataprivacy', component: DataprivacyComponent },
   { path: 'home/logout', component: LogOutComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', component: LogInComponent },
   { path: 'home/scannertest', component: ScannertestComponent }
 ];
 
@@ -53,7 +57,11 @@ const routes: Routes = [
     ListComponent,
     ImpressumComponent,
     ScannertestComponent,
-    DataprivacyComponent
+    DataprivacyComponent,
+    BackButtonComponent,
+    WhatDoesApplComponent,
+    HowCreateListComponent,
+    HowShareListComponent,
   ],
   imports: [
     RouterModule.forRoot(routes, { useHash: true }),
@@ -71,9 +79,7 @@ const routes: Routes = [
     MatSelectModule,
     MatSnackBarModule
   ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
